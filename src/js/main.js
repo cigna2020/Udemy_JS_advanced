933ebfee221cf145ba8444fdc5c02f7343394ea3
Alex <cigan@ua.fm>
2020-10-01 12:57:07 +0300

    lesson 4, add functions for tabs on the page

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import './slider';
 import modals from './modules/modals';
+import tabs from './modules/tabs';
 
 window.addEventListener('DOMContentLoaded', () => {
 
     modals();
+    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');        // active без точки, так как в функции используется classList
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');      // > div > div, так как контент обернут в два дива
 
 });
-
 
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,40 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+    const header = document.querySelector(headerSelector),          // блок, который объединяет все табы
+        tab = document.querySelectorAll(tabSelector),
+        content = document.querySelectorAll(contentSelector);
+
+    function hideTabContent() {
+        content.forEach(item => {
+            item.style.display = 'none';        // скрыли весь контент табов
+        });
+        tab.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+    }
+
+
+    function showTabContent(i = 0) {                 // 0 - чтобы отобразить первый таб при загрузке страницы
+        content[i].style.display = 'block';         // нужно проверить, чтобы в верстке был "блок", "і" - индекс таба
+        tab[i].classList.add(activeClass);
+    }
+    hideTabContent();
+    showTabContent();                                // можно подставить цифру, чтобы отобразить НЕ первый таб при загрузке страницы
+
+
+    // Делегирование событий (отслеживание таба, на который кликнули)
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target &&                       // проверяем таргет, потому-что некоторые элементы не поддержывают событие click                                              
+            (target.classList.contains(tabSelector.replace(/\./, "")) || // проверяем, если кликнули на таб, tabSelector - это класс, поэтому нужно удалить точку
+                target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {  // проверяем, если кликнули не в сам элемент, а в его дечерний элемент
+            tab.forEach((item, i) => {                              //получем индекс таба
+                if (target == item || target.parentNode == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+};
+
+export default tabs;
